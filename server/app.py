390b793caacd6a70554835aef765e85eae42dc7e
david-cl1 <crowndavid32@gmail.com>
2026-08-04 23:35:41 +0300

    minor fixes

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ def home ():
 @app.route ('/models/<model>')
 def models(model):
     if model in existing_models:
-        return f"Flatiron {model} is in our fleet"
+        return f"Flatiron {model} is in our fleet!"
     return f"No models called {model} exists in our catalog", 404
 
 if __name__ == '__main__':
