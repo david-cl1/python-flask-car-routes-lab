b84a7d5e416ba504c5da41250236b05333b89bd1
david-cl1 <crowndavid32@gmail.com>
2026-08-04 23:16:46 +0300

    error fixes

--- server/app.py
+++ server/app.py
@@ -6,13 +6,13 @@ existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
 
 @app.route('/')
 def home ():
-    return "Welcome to Flatiron Cars"
+    return "<p>Welcome to Flatiron Cars<p>"
 
 @app.route ('/<models>')
 def models(model):
     if model in existing_models:
-        return f"Flatiron {model} is in our fleet!"
-    return f"No models called {model} exists in our catalog"
+        return f"<p>Flatiron {model} is in our fleet!<p>"
+    return f"<p>No models called {model} exists in our catalog<p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
